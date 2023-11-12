484682481c0353558baa863cfe3f1585e8acb502
Rigoberto <rigobertobgomez@gmail.com>
2023-11-12 15:12:49 -0600

    another commit

---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import {motion} from "framer-motion"
+
+export const Test = () => {
+  return (
+    <div className="course">
+        <motion.div className="box" initial={{opacity:0, scale:0.5}} 
+        animate={{opacity:1, scale:1, x:200}}
+        transition={{duration:2, }}></motion.div>
+
+    </div>
+  )
+}

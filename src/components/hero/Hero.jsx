f9032017431cc0536a3c1795ce7d924f3d4990ca
Rigoberto <rigobertobgomez@gmail.com>
2023-11-15 20:28:38 -0600

    scroll button and image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,50 @@
 import   './hero.scss'
+import {motion} from "framer-motion"
+
+
+const textVariants = {
+  initial:{
+    x: -500,
+    opacity:0,
+  },
+  animate:{
+    x: 0,
+    opacity:1,
+    transition:{
+      duration: 1,
+      staggerChildren: 0.1,
+    }
+  },
+  scrollButton:{
+    opacity:0,
+    y:10,
+    transition:{
+      duration:2,
+      repeat: Infinity,
+    }
+  }
+}
 
 const Hero = () => {
   return (
     <div className='hero'>
+      <div className="wrapper">
+      <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+        <motion.h2 variants={textVariants}>RIGOBERTO GOMEZ</motion.h2>
+        <motion.h1 variants={textVariants}> Software Developer and Engineer</motion.h1>
+        <motion.div variants={textVariants} className="buttons" >
+          <motion.button variants={textVariants}>See My Work</motion.button>
+          <motion.button variants={textVariants}>Contact Me</motion.button>
+
+        </motion.div>
+        <motion.img variants={textVariants} animate="scrollButton"src="/scroll.png" alt="" />
+      </motion.div>
+      </div>
+      <div className="slidingTextContainer">
+        Educator Motivator Doer
+      </div>
         <div className="imageContainer">
-            <img src="/IMG_9882.png" alt="" />
+            <img className='mePng' src="/IMG_9882.png" alt="" />
         </div>
     </div>
   )
